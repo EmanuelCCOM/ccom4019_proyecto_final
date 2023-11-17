5a3057d985b8577c1c17953c77fca05b67accbd4
Emanuel Martinez <emanuel.martinez8@upr.edu>
2023-11-17 01:39:34 -0400

    Base de datos actualizada

--- 4019_final_p.sql
+++ 4019_final_p.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2023 a las 06:17:54
+-- Tiempo de generación: 17-11-2023 a las 06:38:47
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,24 +24,103 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `admins`
+--
+
+INSERT INTO `admins` (`email`, `pass`) VALUES
+('emanuel.martinez8@upr.edu', '$2y$10$6V2416anpvHjS/5uRtyQD.gOOmzbpl8kfLsRfuCMp3rgS7iWs6Pfa');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `course`
 --
 
 CREATE TABLE `course` (
-  `course_id` varchar(8) NOT NULL,
+  `course_id` char(8) NOT NULL,
   `title` varchar(30) NOT NULL,
   `credits` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `enrollment`
+--
+
+CREATE TABLE `enrollment` (
+  `student_id` char(9) NOT NULL,
+  `course_id` char(8) NOT NULL,
+  `section_id` char(3) NOT NULL,
+  `timestamp` date NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `section`
+--
+
+CREATE TABLE `section` (
+  `course_id` char(8) NOT NULL,
+  `section_id` char(3) NOT NULL,
+  `capacity` int(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` char(9) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `year_of_study` tinyint(1) NOT NULL DEFAULT 1,
+  `name` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `student`
+--
+
+INSERT INTO `student` (`student_id`, `password`, `user_name`, `year_of_study`, `name`, `lastName`) VALUES
+('840199857', 'lkj', 'Emanuel8', 4, 'Emanuel', 'Martinez'),
+('840206745', 'lkj', 'Daisy3', 1, 'Daisy', 'Escalante');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- Indices de la tabla `course`
 --
 ALTER TABLE `course`
   ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indices de la tabla `student`
+--
+ALTER TABLE `student`
+  ADD UNIQUE KEY `student_id` (`student_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
